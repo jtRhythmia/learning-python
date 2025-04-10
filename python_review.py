1253a76bb85ba9c787d612eea241c942af2fbb67
Joshua Tan <joshuatan@Joshuas-MacBook-Pro-9.local>
2025-04-10 09:53:42 -0400

    Problem 5 Complete

--- python_review.py
+++ python_review.py
@@ -24,3 +24,55 @@ A variable name cannot be any of the Python keywords.
 
 '''
 
+
+# prompt = "Enter your name"
+# response = input(prompt)
+# print("Hello,", response)
+# print(response, ", here is your agenda for the day...")
+
+
+
+
+# prompt_one = "Enter your name:"
+# response_one = input(prompt_one)
+
+# prompt_two = "Enter your age:"
+# response_two = input(prompt_two)
+
+# print(response_one, type(response_one))
+# print(response_two, type(response_two))
+
+# Problem 2
+# Ask the user for the two numbers
+# first_number = input("What's the first number?")
+# second_number = input("What's the second number?")
+
+# Convert the numbers to integer versions of themselves
+# first_number = int(first_number)
+# second_number = int(second_number)
+
+# Compare the two numbers
+# if first_number > second_number:
+#     print(first_number, "is bigger.")
+
+# elif first_number == second_number:
+#     print("The numbers are equal")
+
+# else:
+#     print(second_number, "is bigger.")
+
+# Problem 5
+# Ask the user for their name and save in variable called "name"
+name = input("What's your full name?")
+while " " in name:
+    name = name.replace(" ", "")
+    
+# Calculate the length of name
+length_of_name = len(name)
+
+# Print the length
+print(length_of_name)
+
+
+
+
