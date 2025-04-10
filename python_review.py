fcd958904a9dfd1c957e8bc440b031a64d2c7445
Joshua Tan <joshuatan@Joshuas-MacBook-Pro-9.local>
2025-04-10 09:14:48 -0400

    Level 4 Complete

--- python_review.py
+++ python_review.py
@@ -1,2 +1,16 @@
+# Name: Joshua Tan
+
 print("It worked!")
 
+x = None
+
+example = "Im a string" # The variable called [example] is now holding the [string] value ["I'm a string!"]
+a = 3 # The variable called [a] is now holding the [integer] value [3]
+b = 4.0 # The variable called [b] is now holding the [float] value [4.0]
+c = True # The variable called [c] is now holding the [Boolean] value [True]
+d = False # The variable called [d] is now holding the [Boolean] value [False]
+e = "Hey!" # The variable called [e] is now holding the [string] value ["Hey!"]
+f = None # The variable called [f] is not holding any value
+age = 32 # The variable called [age] is now holding the [integer] value [32]
+name = "Insert your name here" # The variable called [name] is now holding the [string] value ["Insert your name here"]
+instrument = "Insert your instrument here" # The variable called [name] is now holding the [string] value ["Insert your instrument here"]
